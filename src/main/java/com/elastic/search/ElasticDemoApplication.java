ef0c02631314e6699a81b4514926c6371f758378
吴相帅 <1210434385@qq.com>
2022-04-02 14:38:33 +0800

    elasticDemo

---
+++ src/main/java/com/elastic/search/ElasticDemoApplication.java
@@ -0,0 +1,13 @@
+package com.elastic.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticDemoApplication {
+    // https://blog.csdn.net/u011863024/article/details/115721328
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticDemoApplication.class, args);
+    }
+
+}
